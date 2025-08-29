76c0345805c820d7187db0fa9f918a55703e91af
Maharana pratap <maharanapratap2004@gmail.com>
2025-08-29 23:37:14 +0530

    Add bubble sort implementation

---
+++ bubble_sort.cpp
@@ -0,0 +1,39 @@
+/*
+Bubble Sort Algorithm
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+Stable: Yes
+Best Case: O(n) when array is already sorted
+*/
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped)
+            break;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, n);
+    cout << "Sorted array: ";
+    printArray(arr, n);
+    return 0;
+}
